67f89da5b957314a13997045543929ff40ed823f
Cyanbeacon <86344464+Cyanbeacon@users.noreply.github.com>
2021-08-23 15:23:32 +0800

    Create learn py hard way1.py
    try 1

---
+++ learn py hard way1.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("hello world")
+print("hello again")
+print("i like type this")
+print('yay! printing')
+#print("i'd much rather you 'not'.")
+#print('i"said" do not touch this')
+
+
+# In[4]:
+
+
+print("I will now conut my chekens:")
+
+print("Hens", 25 + 3 / 6)
+
+
+# In[6]:
+
+
+print("NOW I will conut the eggs:" )
+print(3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+
+
+# In[22]:
+
+
+print(1 - 5 + 4 % 2  )
+
+
+# In[30]:
+
+
+print(100 - 25 * 3%4)
+
+
+# In[31]:
+
+
+print(3+2+1-5+4%2-1/4+6)
+
+
+# In[40]:
+
+
+print(3+2<5-7)
+
+print(3+2<5-7)
+
+
+# In[41]:
+
+
+print("is it true that 5+2>3+1?")
+
+
+# In[42]:
+
+
+print(5+2>3+1)
+
+
+# In[45]:
+
+
+print(5>=2)
+
+
+# In[57]:
+
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+print("There are", cars, "car available.")
+print("there are only", drivers, "drivers available.")
+print("there will be", cars_not_driven, "empty cars today.")
+print("we can transport", carpool_capacity, "people today.")
+print("we have", passengers, "to carpool today.")
+print("we need to put about", average_passengers_per_car,"in each car.")
+
+
+# In[56]:
+
+
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+print("There are", cars, "car available.")
+print("there are only", drivers, "drivers available.")
+print("there will be", cars_not_driven, "empty cars today.")
+print("we can transport", carpool_capacity, "people today.")
+print("we have", passengers, "to carpool today.")
+print("we need to put about", average_passengers_per_car,"in each car.")
+
+
+# In[64]:
+
+
+name = 'zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"et's talk about {name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} pounds heavy")
+print("Actrully that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+
+# In[69]:
+
+
+name = 'zed A. Shaw'
+my_age = 35 # not a lie
+my_height_inche = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+my_height = my_height_inche * 30.48
+
+print(f"et's talk about {name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} pounds heavy")
+print("Actrully that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+
+# In[ ]:
+
+
+
+
